76537e9c3edcbcff1ca25d560d074eba80f26918
Mohamed Hossny <mhossny2@outlook.com>
2024-12-02 01:22:53 +0200

    feat: implement Write-Ahead Log (WAL) interface and add Record struct

---
+++ wal.go
@@ -0,0 +1,13 @@
+package s3log
+
+import "context"
+
+type Record struct {
+	Offset uint64
+	Data   []byte
+}
+
+type WAL interface {
+	Append(ctx context.Context, data []byte) (uint64, error)
+	Read(ctx context.Context, offset uint64) (Record, error)
+}
